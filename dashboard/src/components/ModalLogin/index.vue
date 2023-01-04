f114a91f79bee292084d6268d651a4e62b36e3d3
Sol Oliveira <sol.oliveira@elevenfinancial.com>
2023-01-04 14:43:30 -0300

    add: modal login

--- dashboard/src/components/ModalLogin/index.vue
+++ dashboard/src/components/ModalLogin/index.vue
@@ -1,13 +1,101 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div>
-    <h1>Modal de Login</h1>
+  <div class="flex justify-between" id="modal-login">
+    <h1 class="text-4xl font-black text-gray-800">
+      Entre na sua conta
+    </h1>
 
+    <button
+      @click="close"
+      class="text-4xl text-gray-600 focus:outline-none"
+    >
+      &times;
+    </button>
+  </div>
+
+  <div class="mt-16">
+    <form @submit.prevent="handleSubmit">
+      <label class="block">
+        <span class="text-lg font-medium text-gray-800">E-mail</span>
+        <input
+          id="email-field"
+          v-model="state.email.value"
+          type="email"
+          :class="{
+            'border-brand-danger': !!state.email.errorMessage
+          }"
+          class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
+          placeholder="meuemail@email.com"
+        >
+        <span
+          id="email-error"
+          v-if="!!state.email.errorMessage"
+          class="block font-medium text-brand-danger"
+        >
+          {{ state.email.errorMessage }}
+        </span>
+      </label>
+
+      <label class="block mt-9">
+        <span class="text-lg font-medium text-gray-800">Senha</span>
+        <input
+          id="password-field"
+          v-model="state.password.value"
+          type="password"
+          :class="{
+            'border-brand-danger': !!state.password.errorMessage
+          }"
+          class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
+          placeholder="*********"
+        >
+        <span
+          v-if="!!state.password.errorMessage"
+          class="block font-medium text-brand-danger"
+        >
+          {{ state.password.errorMessage }}
+        </span>
+      </label>
+
+      <button
+        id="submit-button"
+        :disabled="state.isLoading"
+        type="submit"
+        :class="{
+          'opacity-50': state.isLoading
+        }"
+        class="px-8 py-3 mt-10 text-2xl font-bold text-white rounded-full bg-brand-main focus:outline-none transition-all duration-150"
+      >
+        <icon v-if="state.isLoading" name="loading" class="animate-spin" />
+        <span v-else>Entrar</span>
+      </button>
+    </form>
   </div>
 </template>
 
 <script setup>
+import useModal from '@/hooks/useModal';
+import { reactive } from 'vue';
 
+const state = reactive({
+  hasErrors: false,
+  isLoading: false,
+  email: {
+    value: '',
+    errorMessage: ''
+  },
+  password: {
+    value: '',
+    errorMessage: ''
+  }
+})
+
+const modal = useModal()
+
+const close = () => {
+  return modal.close();
+}
+
+const handleSubmit  = () => { }
 </script>
 
 <style lang="scss" scoped>
