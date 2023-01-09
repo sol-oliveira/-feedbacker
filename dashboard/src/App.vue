08019b1174f941fbef4cddae128bbdaff6d6b96d
Sol Oliveira <sol.oliveira@elevenfinancial.com>
2023-01-09 17:08:33 -0300

    add: rota autenticação

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,5 +1,23 @@
 <script setup >
+import { watch } from 'vue'
 import ModalFactory  from '@/components/ModalFactory';
+import { useRouter, useRoute } from 'vue-router';
+import services from './services';
+
+const router = useRouter()
+const route = useRoute()
+
+watch(() => route.path, async () => {
+  if (route.meta.hasAuth) {
+    const token = window.localStorage.getItem('token')
+    if (!token) {
+      router.push({ name: 'Home' })
+      return
+    }
+    const { data } = await services.users.getMe()
+    console.log(data);
+  }
+})
 
 </script>
 <template>
