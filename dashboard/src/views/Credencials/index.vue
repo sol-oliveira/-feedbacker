e2beeaef49fe139e4000ff44dd7727e6ad3e7a42
Sol Oliveira <sol.oliveira@elevenfinancial.com>
2023-01-27 19:00:25 -0300

    create: tela credenciais/3

--- dashboard/src/views/Credencials/index.vue
+++ dashboard/src/views/Credencials/index.vue
@@ -21,19 +21,22 @@
       Este aqui é a sua chave de api
     </p>
 
-    <div class="flex py-3 pl-5 mt-2 rounded justify-between items-center bg-brand-gray w-full lg:w-1/2">
-     <span>{{ store.currentUser.apiKey }}</span>
+    <content-loader v-if="store.Global.isLoading || state.isLoading" class="rounded" width="600px" height="50px" />
+
+    <div v-else class="flex py-3 pl-5 mt-2 rounded justify-between items-center bg-brand-gray w-full lg:w-1/2">
+     <span>{{ store.User.currentUser.apiKey }}</span>
      <div class="flex ml-20 mr-5">
-      <Icon name="Copy" :color="brand.graydark" :size="24" class="cursor-pointer" />
-      <Icon name="Loading" :color="brand.graydark" :size="24" class="cursor-pointer ml-3" />
+      <icon name="Copy" :color="brand.graydark" :size="24" class="cursor-pointer" />
+      <icon name="Loading" :color="brand.graydark" :size="24" class="cursor-pointer ml-3" />
      </div>
     </div>
     <p class="mt-5 text-lg text-gray-800 font-regular">
-        Coloque o script abaixo no seu site para começar a receber feedbacks
-      </p>
-      <div class="py-3 pl-5 pr-20 mt-2 rounded bg-brand-gray w-full lg:w-2/3 overflow-x-scroll">
+      Coloque o script abaixo no seu site para começar a receber feedbacks
+    </p>
+    <content-loader v-if="store.Global.isLoading || state.isLoading" class="rounded" width="600px" height="50px" />
+    <div v-else class="py-3 pl-5 pr-20 mt-2 rounded bg-brand-gray w-full lg:w-2/3 overflow-x-scroll">
       <pre>&lt;script src="https://sol-oliveira-feedbacker-widget.netlify.app?api_key=fcd5015c-10d3-4e9c-b395-ec7ed8850165" &gt;&lt;/script&gt;</pre>
-      </div>
+    </div>
   </div>
 
 </div>
@@ -41,14 +44,22 @@
 </template>
 
 <script setup>
+
+import { reactive } from 'vue'
+import useStore from '@/hooks/useStore'
+
 import HeaderLogged from '@/components/HeaderLogged'
 import  Icon  from '@/components/Icon'
-
-import useStore from '@/hooks/useStore'
+import ContentLoader from "@/components/ContentLoader"
 import palette from '../../../palette'
 
-const store = useStore('User')
+const store = useStore()
 const { brand } = palette;
+
+const state = reactive({
+  hasError: false,
+  isLoading: false
+});
 
 </script>
 
